bc31442bfa51654a3f791c3602381dc34249a247
Jakub741 <kuba.ki@seznam.cz>
2022-06-18 15:00:57 +0200

    pokus č.32325

---
+++ Kuba-pokus/App.js
@@ -0,0 +1,8 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./components/table";
+function App() {
+  return (
+    <table />
+  );
+  }
+export default App;
