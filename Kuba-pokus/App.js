3fa7aab2119b23e397fbb5309bddf53555d92f51
Yar1x <richtrik.j@gmail.com>
2022-06-23 08:03:07 +0200

    Uprava kódu

--- Kuba-pokus/App.js
+++
@@ -1,8 +0,0 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import DynamicTable from "./components/table";
-function App() {
-  return (
-    <table />
-  );
-  }
-export default App;
--- Kuba-pokus/components/data.js
+++
@@ -1,9 +0,0 @@
-const TableData=[
-    {id:1, Name:"Adam novak", termin1:"E", termin2:"A", termin3:"Neprihlasen"},
-    {id:1, Name:"Adam novak", termin1:"E", termin2:"A", termin3:"Neprihlasen"},
-    {id:1, Name:"Adam novak", termin1:"E", termin2:"A", termin3:"D"},
-    {id:1, Name:"Adam novak", termin1:"E", termin2:"A", termin3:"Neprihlasen"},
-    {id:1, Name:"Adam novak", termin1:"E", termin2:"A", termin3:"Neprihlasen"},
-    {id:1, Name:"Adam novak", termin1:"C", termin2:"A", termin3:"Neprihlasen"},
-]
-export default TableData;
